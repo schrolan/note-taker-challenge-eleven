bf8c975cb4ad17cf7c1cf513bdc563706bde625c
schrolan <schrolan000@gmail.com>
2023-10-01 20:24:16 -0500

    I created a routes folder and filled out the server.js file.

---
+++ server.js
@@ -0,0 +1,13 @@
+const routes = require('./routes')
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+app.use(express.static('public'))
+app.use(express.join())
+
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:3000 ${PORT}`)
+})
